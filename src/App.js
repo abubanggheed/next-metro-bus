7f3f75d8c68185cd33d5a24560f580c70c24759c
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-03-25 14:22:35 -0500

    completed basic form infrastructure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    route: '',
+    stopName: '',
+    direction: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h2>Next Bus</h2>
         </header>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Route: {' '}
+            <input name="route" type="text"
+              onChange={this.handleChange}
+              value={this.state.route}
+            />
+          </label>
+          <br />
+          <label>Stop: {' '}
+            <input name="stopName" type="text"
+              onChange={this.handleChange}
+              value={this.state.stopName}
+            />
+          </label>
+          <br />
+          <label>Direction: {' '}
+            <input name="direction" type="number"
+              onChange={this.handleChange}
+              value={this.state.direction}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Get Departure Time" />
+        </form>
       </div>
     );
   }
